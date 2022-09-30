c5da24256de1ea93e9dcf1dcc17e31e13411051b
Ivan Miller <therealigmiller@gmail.com>
2022-10-01 00:23:20 +0300

    feat: add renderTAgs

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,25 +1,43 @@
 import React, {useState} from "react"
 
 const Counter = () => { 
-    const [count, setCounter] = useState(0);
-
-    const formatCount = () => { 
+    const [count, setCount] = useState(0);
+    const [tags, setTags] = useState(['tag1', 'tag2',' tag3'])
+    const formatCount = () => {
         return count === 0 ? 'empty' : count;
     }
 
 
     const handleIncrement = () => {
-        setCounter(count + 1)
-        console.log(count);
+        setCount((prevState) => prevState + 1)
     }
     
     const handleDicrement = () => {
-        setCounter(count - 1)
-        console.log(count);
+        setCount((prevState) => prevState - 1)
     }
- 
+
+    const handleChange = () => {
+        return setTags(['tag4', 'tag5'])
+    }
+
+
+    const renderTags = () => {
+        if(tags.length == 0) {
+            return 'No tags';
+        }
+        return tags.map((tag) => {
+            return <li
+                key={tag}
+                className="btn btn-primary btn-sm"
+                onClick={handleChange}>
+                {tag}</li>
+        })
+    }
     return (
-            <> 
+            <>
+                <ul>
+                    {}
+                </ul>
                 <button className="btn btn-primary btn-sm m-2" onClick={handleDicrement}>-</button>
                 <span className="badge m-2 bg-primary">{formatCount()}</span>
                 <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import reactDom from "react-dom";
+// import reactDom from "react-dom";
+import {createRoot} from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css"
 import Counter from "./components/counter";
 
@@ -7,5 +8,5 @@ const App = () => {
     return <Counter/>
 }
 
-reactDom.render(<App/>, document.getElementById('root'));
-
+// reactDom.render(<App/>, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App/>);
