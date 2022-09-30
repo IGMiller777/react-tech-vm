ddbc574fc47a0bee8d0941610b051385cb1800cc
Ivan <igamanovich@elinext.com>
2022-09-30 19:04:31 +0300

    feat: add render state method

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -9,13 +9,18 @@ const Counter = () => {
 
 
     const handleIncrement = () => {
-        setCounter(count +1)
+        setCounter(count + 1)
         console.log(count);
-
+    }
+    
+    const handleDicrement = () => {
+        setCounter(count - 1)
+        console.log(count);
     }
  
     return (
             <> 
+                <button className="btn btn-primary btn-sm m-2" onClick={handleDicrement}>-</button>
                 <span className="badge m-2 bg-primary">{formatCount()}</span>
                 <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
             </> 
