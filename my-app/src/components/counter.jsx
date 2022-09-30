be539bca927c7f99c97b00c6752669e814be52bd
Ivan <igamanovich@elinext.com>
2022-09-30 18:59:13 +0300

    feat: add use state

---
+++ my-app/src/components/counter.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react"
+
+const Counter = () => { 
+    const [count, setCounter] = useState(0);
+
+    const formatCount = () => { 
+        return count === 0 ? 'empty' : count;
+    }
+
+
+    const handleIncrement = () => {
+        setCounter(count +1)
+        console.log(count);
+
+    }
+ 
+    return (
+            <> 
+                <span className="badge m-2 bg-primary">{formatCount()}</span>
+                <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
+            </> 
+        )
+
+} 
+
+export default Counter;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"
+import Counter from "./components/counter";
 
-const element = <h1>Hello World!</h1>
+const App = () => { 
+    return <Counter/>
+}
 
-reactDom.render(element, document.getElementById('root'));
+reactDom.render(<App/>, document.getElementById('root'));
 
-console.log(element)
