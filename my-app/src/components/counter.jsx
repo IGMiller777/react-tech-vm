c7980e47723a506cac74b7548eae67f24e05bcaa
Ivan Miller <therealigmiller@gmail.com>
2022-10-02 18:07:05 +0300

    feat: add new use state compile

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,34 +1,39 @@
 import React, {useState} from "react"
 
 const Counter = (props) => { 
-    const [value, setValue] = useState(props.value);
 
+    const {value} = props
     const formatCount = () => {
         return value === 0 ? 'empty' : value;
     }
 
     const handleIncrement = () => {
-        setValue((prevState) => prevState + 1)
+        // setValue((prevState) => prevState + 1)
     }
     
     const handleDicrement = () => {
-        setValue((prevState) => prevState - 1)
+        // setValue((prevState) => prevState - 1)
     }
 
     const handleChange = () => {
-        return setValue(['tag4', 'tag5'])
+        // return setValue(['tag4', 'tag5'])
     }
 
 
     return (
             <div>
-                {props.children}
+                <span>{props.name}</span>
                 <ul>
                     {}
                 </ul>
                 <button className="btn btn-primary btn-sm m-2" onClick={handleDicrement}>-</button>
                 <span className="badge m-2 bg-primary">{formatCount()}</span>
                 <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
+                <button 
+                    className="btn btn-danger btn-sm m-2" 
+                    onClick={() => props.onDelete(props.id)}>
+                        Delete
+                </button>
             </div> 
         )
 
